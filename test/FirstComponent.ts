effe8b3df9be414f79e099733191a400f7f37493
Simon890 <revellosimon890@gmail.com>
2023-03-02 14:47:49 -0300

    Added first form example

--- test/FirstComponent.ts
+++ test/FirstComponent.ts
@@ -1,21 +1,33 @@
 import { Component } from "../src/Component";
-import htmlTemplate from "./htmlTemplate.html";
+import html from "./firstComponent.html";
 
 type Data = {
-    name: string
+    name: string,
+    password: string
 }
 
 export class FirstComponent extends Component<Data> {
     constructor() {
         super({
             data: {
-                name: "Simon"
+                name: "",
+                password: ""
             },
-            html: htmlTemplate
+            html
         });
     }
     
     handleClick() {
-        console.log("CLICKED FROM COMPONENT", this.data.name);
+        if(this.data.name.length < 5) return alert("Name must have at least five characters");
+        if(this.data.password.length < 8) return alert("Password must have at least eight characters");
+        alert(`Login success for user ${this.data.name}`);
+    }
+
+    handleNameChange(e: any) {
+        this.data.name = e.target.value;
+    }
+
+    handlePasswordChange(e: any) {
+        this.data.password = e.target.value;
     }
 }
